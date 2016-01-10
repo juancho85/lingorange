91a78a364530ac35b734a2cec5253a007127d9bd
Juan David Sánchez <juancho85@gmail.com>
2016-01-10 12:31:30 +0100

    create a task for each type of file changed

--- src/main/ui/gulpfile.js
+++ src/main/ui/gulpfile.js
@@ -4,6 +4,10 @@ var TS_PATHS = {
     src: 'src/**/*.ts'
 };
 
+var JSON_PATHS = {
+    src: 'src/**/*.json'
+};
+
 var HTML_PATHS = {
     src: 'src/**/*.html'
 };
@@ -22,6 +26,7 @@ gulp.task('clean', function (done) {
 });
 
 gulp.task('ts2js', ['copy-static'], function () {
+    debug = true;
     var typescript = require('gulp-typescript');
     var tscConfig = require('./tsconfig.json');
 
@@ -32,9 +37,21 @@ gulp.task('ts2js', ['copy-static'], function () {
     return tsResult.js.pipe(gulp.dest(JS_DESTINATION));
 });
 
+gulp.task('copy-html', function(){
+    gulp.src([HTML_PATHS.src]).pipe(gulp.dest(JS_DESTINATION));
+});
+
+gulp.task('copy-css', function(){
+    gulp.src([CSS_PATHS.src]).pipe(gulp.dest(JS_DESTINATION));
+});
+
+gulp.task('copy-json', function(){
+    gulp.src([JSON_PATHS.src]).pipe(gulp.dest(JS_DESTINATION));
+});
+
 gulp.task('copy-static', function () {
     //html & css
-    gulp.src([HTML_PATHS.src, CSS_PATHS.src]).pipe(gulp.dest(JS_DESTINATION));
+    gulp.src([HTML_PATHS.src, CSS_PATHS.src, JSON_PATHS.src]).pipe(gulp.dest(JS_DESTINATION));
 });
 
 gulp.task('play', ['ts2js'], function () {
@@ -46,8 +63,9 @@ gulp.task('play', ['ts2js'], function () {
     var port = 9000, app;
 
     gulp.watch(TS_PATHS.src, ['ts2js']);
-    gulp.watch(HTML_PATHS.src, ['copy-static']);
-    gulp.watch(CSS_PATHS.src, ['copy-static']);
+    gulp.watch(HTML_PATHS.src, ['copy-html']);
+    gulp.watch(CSS_PATHS.src, ['copy-css']);
+    gulp.watch(JSON_PATHS.src, ['copy-json']);
 
     //app = connect().use(serveStatic(__dirname));
     app = connect().use(serveStatic(INDEX_LOCATION));
